cc98c0e519bc83e2975c5333ffc863cafb61d07d
huzhihui <wonderfulhzh@163.com>
2018-04-09 09:29:09 +0800

    编辑podspec

---
+++ CTMediator/CTMeditor.swift
@@ -0,0 +1,13 @@
+//
+//  CTMeditor.swift
+//  CTMediator_demo
+//
+//  Created by 胡志辉 on 2018/4/9.
+//  Copyright © 2018年 胡志辉. All rights reserved.
+//
+
+import UIKit
+
+class CTMeditor: NSObject {
+
+}
